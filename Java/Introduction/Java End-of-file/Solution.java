d311b7c349c9a4f955246d7c547066633b680bf8
Lahiru Lomitha <42784437+lomitha96@users.noreply.github.com>
2018-10-04 13:01:08 +0530

    Create Solution.java

---
+++ Java/Introduction/Java End-of-file/Solution.java
@@ -0,0 +1,14 @@
+ 
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = 1;
+        while (scan.hasNextLine()) {
+            System.out.println(i + " " + scan.nextLine());
+            i++;
+        }
+        scan.close();
+    }
+}
