f5fd2d8e1bfb3dc89db156b2cfe37caf285b8ae9
Lahiru Lomitha <42784437+lomitha96@users.noreply.github.com>
2018-10-04 12:58:24 +0530

    Create Solution.java

---
+++ Java/Introduction/Java Loops I/Solution.java
@@ -0,0 +1,19 @@
+ 
+import java.util.*; 
+
+public class Solution {
+    
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d%n", N, i , i * N);
+        }
+
+    }
+}
